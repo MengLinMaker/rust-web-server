d5a53ecf716dfe85b0183e372fb70e1fbcf4b7ba
MengLinMaker <menglinmaker@gmail.com>
2026-08-07 15:39:42 +1000

    feat: serve html

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use std::{
-    io::{BufRead, BufReader},
-    net::{TcpListener, TcpStream},
+    fs, io::{BufRead, BufReader, Write}, net::{TcpListener, TcpStream},
 };
 
 fn main() {
@@ -15,7 +14,7 @@ fn main() {
     }
 }
 
-fn handle_connection(stream: TcpStream) {
+fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&stream);
     let http_request: Vec<_> = buf_reader
         .lines()
@@ -26,4 +25,11 @@ fn handle_connection(stream: TcpStream) {
     let http_length = http_request.len();
     tracing::info!("handle_connection - http_length: {http_length}");
     tracing::trace!("handle_connection - http_request: {http_request:#?}");
+
+    let status_line = "HTTP/1.1 222 CUSTOM STATUS";
+    let content = fs::read_to_string("src/hello.html").unwrap();
+    let length = content.len();
+    // HTTP format is just a text file with key value pairs except status and content
+    let response = format!("{status_line}\nContent-length: {length}\n\n{content}");
+    stream.write_all(response.as_bytes()).unwrap();
 }
